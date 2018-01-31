bc2a06351d54fc35030cf00c31751d28885d7944
getmaxed <maxfaden07@gmail.com>
2018-01-31 14:36:17 -0800

    Working on displaying tweet popups

--- home.php
+++ home.php
@@ -204,8 +204,9 @@
                         </div>
                         <div class="popupTweet"></div>
                         <!--Tweet END WRAPER-->
-                    <script type="text/javascript" src="assets/js/like.js"></script>
+                        <script type="text/javascript" src="assets/js/like.js"></script>
                         <script type="text/javascript" src="assets/js/retweet.js"></script>
+                        <script type="text/javascript" src="assets/js/popuptweets.js"></script>
                     </div><!-- in left wrap-->
                 </div><!-- in center end -->
 
