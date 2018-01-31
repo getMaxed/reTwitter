c5de303c7eb9250dbdaf4626fc4a559f10b91839
getmaxed <maxfaden07@gmail.com>
2018-01-31 13:16:40 -0800

    Retweet and display retweets methods created

--- assets/js/retweet.js
+++ assets/js/retweet.js
@@ -1,16 +1,26 @@
 $(function() {
     $(document).on('click', '.retweet', function() {
-        var tweet_id = $(this).data('tweet');
-        var user_id = $(this).data('user');
+        $tweet_id = $(this).data('tweet');
+        $user_id = $(this).data('user');
         $counter = $(this).find('.retweetsCount');
         $count = $counter.text();
         $button = $(this);
 
-        $.post('http://coe.dev/123/projects/xxx/reTwitter/core/ajax/retweet.php', {showPopup:tweet_id, user_id:user_id}, function(data) {
+        $.post('http://coe.dev/123/projects/xxx/reTwitter/core/ajax/retweet.php', {showPopup:$tweet_id, user_id:$user_id}, function(data) {
             $('.popupTweet').html(data);
             $('.close-retweet-popup').click(function () {
                 $('.retweet-popup').hide();
             });
         });
     });
+
+    $(document).on('click', '.retweet-it', function () {
+        var comment = $('.retweetMsg').val();
+        $.post('http://coe.dev/123/projects/xxx/reTwitter/core/ajax/retweet.php', {retweet:$tweet_id, user_id:$user_id, comment:comment}, function () {
+            $('.retweet-popup').hide();
+            $count++;
+            $counter.text($count);
+            $button.removeClass('retweet').addClass('retweeted');
+        })
+    })
 });
